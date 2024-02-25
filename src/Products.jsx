40b664e94629b84da4b60f07e9285573022491f7
suryanalam <suryanalam3011@gmail.com>
2024-02-25 23:29:27 +0530

    implemented pagination

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,35 +1,35 @@
-// Logic if limit, skip and total properties are "not present" in the given API
+// Logic if limit, skip and total properties are "present" in the given API
 
 import "./App.css";
 import { useEffect, useState } from "react";
 
-function App() {
+const Products = () => {
   const [products, setProducts] = useState([]);
-  const [totalPagesCount, setTotalPagesCount] = useState(0);
+  const [totalPagesCount, setTotalPagesCount] = useState(1);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
-
     let fetchProducts = async () => {
-      let resp = await fetch("https://dummyjson.com/products?limit=0");
+      let resp = await fetch(
+        `https://dummyjson.com/products?limit=15&skip=${page * 15 - 15}`
+      );
       let data = await resp.json();
       console.log("resp from api: ", data);
-  
+
       if (data?.products) {
         setProducts(data.products);
-        setTotalPagesCount(Math.ceil(products.length / 15))
+        setTotalPagesCount(Math.ceil(data.total / 15));
       }
     };
-
     fetchProducts();
-  }, [page, products.length]);
+  }, [page]);
 
   return (
     <main>
       <header>Pagination</header>
       {products.length > 0 && (
         <div className="products-div">
-          {products.slice(page * 15 - 15, page * 15).map((product) => {
+          {products.map((product) => {
             return (
               <section key={product.id} className="product-bg">
                 <img src={product.thumbnail} alt={product.title} />
@@ -67,6 +67,6 @@ function App() {
       )}
     </main>
   );
-}
+};
 
-export default App;
+export default Products;
